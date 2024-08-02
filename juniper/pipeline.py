ca355549dfab28873f3955888a818c267b4c4e29
AbbyBoehm <vab55@cornell.edu>
2024-08-02 15:53:36 -0400

    Stage 2 done, expanding to add Stage 3

--- juniper/pipeline.py
+++ juniper/pipeline.py
@@ -82,17 +82,18 @@ def run_pipeline(config_folder,stages=(1,2,3,4,5,6,)):
 
         # Find files.
         files = sorted(glob.glob(os.path.join(input_dir,"*rateints.fits")))
-        outfiles = [None for i in files] # use default names and change rateints to calints, that's all.
+        fnames = [str.split(f,sep='/')[-1] for f in files]
+        outfiles = [str.replace(f,'_rateints.fits','_calints') for f in fnames] # use default names and change rateints to calints, that's all.
         if s2_config["rename"]:
             # Set up new outfile names and also change rateints to calints.
             outfiles = ['{}_f{}_calints'.format(s2_config["rename"],i) for i, f in enumerate(files)]
 
         # Set up crds cache.
-        os.environ["CRDS_PATH"] = "crds_cache/jwst_ops" # if no path to crds_cache defined, set it up in the cwdir
+        os.environ["CRDS_PATH"] = "crds_cache/jwst_ops/" # if no path to crds_cache defined, set it up in the cwdir
         if s2_config["CRDS_PATH"]:
             os.environ["CRDS_PATH"] = s2_config["CRDS_PATH"] # set path to CRDS cache if you already have on
-        elif not os.path.exists("crds_cache"):
-            os.makedirs("crds_cache/jwst_ops")
+        elif not os.path.exists("crds_cache/jwst_ops/"):
+            os.makedirs("crds_cache/jwst_ops/")
 
         # Process Stage 2.
         do_stage2(files,outfiles,output_dir,s2_config,diagnosticplots_dir)
@@ -101,5 +102,42 @@ def run_pipeline(config_folder,stages=(1,2,3,4,5,6,)):
         config_outdir = os.path.join(output_dir,"configuration")
         if not os.path.exists(config_outdir):
             os.makedirs(config_outdir)
-        config_name = "s2_{}_juniper.berry".format(run_name)
+        config_name = "s2_juniper.berry"
+        if run_name:
+            config_name = "s2_{}_juniper.berry".format(run_name)
         shutil.copy(s2_config_path,os.path.join(config_outdir,config_name))
+    ### Run Juniper Stage 3: Reduction
+    if 3 in stages:
+        # Open the config dictionary.
+        s3_config_path = glob.glob(os.path.join(config_folder,'s3_*'))[0]
+        s3_config = read_config.read_config(s3_config_path)
+
+        # Set up run name and define directories.
+        run_name = s3_config["run_name"] # set up run_name for the .berry file
+        project_dir = s3_config["toplevel_dir"]
+        input_dir = os.path.join(project_dir,s3_config["input"])
+        output_dir = os.path.join(project_dir,s3_config["output"])
+        if run_name:
+            # Add an extra sub-folder to separate this run from other runs.
+            output_dir = os.path.join(project_dir,os.path.join(s3_config["output"],run_name))
+        diagnosticplots_dir = os.path.join(project_dir, s3_config["diagnostics"])
+
+        # Find files.
+        files = sorted(glob.glob(os.path.join(input_dir,"*calints.fits")))
+        fnames = [str.split(f,sep='/')[-1] for f in files]
+        outfiles = [str.replace(f,'_calints.fits','_reduced') for f in fnames] # use default names and change rateints to calints, that's all.
+        if s3_config["rename"]:
+            # Set up new outfile names and also change rateints to calints.
+            outfiles = ['{}_f{}_reduced'.format(s3_config["rename"],i) for i, f in enumerate(files)]
+
+        # Process Stage 3.
+        do_stage3(files,outfiles,output_dir,s3_config,diagnosticplots_dir)
+
+        # Write the config dictionary out as a copy.
+        config_outdir = os.path.join(output_dir,"configuration")
+        if not os.path.exists(config_outdir):
+            os.makedirs(config_outdir)
+        config_name = "s3_juniper.berry"
+        if run_name:
+            config_name = "s3_{}_juniper.berry".format(run_name)
+        shutil.copy(s3_config_path,os.path.join(config_outdir,config_name))
